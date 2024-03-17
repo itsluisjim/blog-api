9ec4db12340c1ea0df4392f1873e7445c1584aaf
Luis <eternal_pitta0b@icloud.com>
2024-03-16 23:09:27 -0500

    update model

--- models/comment.js
+++ models/comment.js
@@ -5,6 +5,7 @@ const CommentSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   createdAt: { type: Date, default: Date.now },
   comment: { type: String, require: true },
+  post_id: { type: Schema.Types.ObjectId, ref: "Post"}
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);
