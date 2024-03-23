5046b0679283a691467e3b4e565750237316e83d
Luis <eternal_pitta0b@icloud.com>
2024-03-23 00:40:30 -0500

    add route to update comments

--- routes/comment.js
+++ routes/comment.js
@@ -9,5 +9,6 @@ router.post('/create',comment_controller.create_comment);
 // Admin Routes
 router.get('/:id', comment_controller.get_comment_detail);
 router.delete('/:id/delete', comment_controller.delete_comment);
+router.put('/:id/update', comment_controller.update_comment)
 
 module.exports = router;
