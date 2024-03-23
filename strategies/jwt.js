edcca1a40072c602d65cc299561f654770bcbc4a
Luis <eternal_pitta0b@icloud.com>
2024-03-22 20:06:31 -0500

    Implement own jwt strategy

---
+++ strategies/jwt.js
@@ -0,0 +1,23 @@
+const JwtStrategry = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
+require('dotenv').config();
+
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.SECRET
+};
+
+
+module.exports = new JwtStrategry(opts, async (JwtPayload, done) => {
+    const userId = JwtPayload.user._id;
+
+    const user = await User.findById(userId).exec();
+
+    if (!user) {
+      return done(null, false);
+    } else {
+        return done(null, user)
+    }
+})
